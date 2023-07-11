dd55650dde866993a0702e06f4289f5143d089a6
alexandre josé de santana melo <138142928+Alexandremelo32@users.noreply.github.com>
2023-07-11 15:25:46 -0300

    Atualização dos desafios

--- desafios/d004/conversao.php
+++ desafios/d004/conversao.php
@@ -11,8 +11,14 @@
     <main>
         <h1>Conversor de Moedas</h1>
     <?php 
-        // Cotação copiada do Google
-        $cotação = 4.89;
+        // Cotação vinda da API do Banco Central
+        $início = date("m-d-Y", strtotime("-7 days"));
+        $fim = ("m-d-Y");
+        $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $início .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$orderby=dataHoraCotacao&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+    
+        $dados = json_decode(file_get_contents($url), true);
+    
+        $cotação = $dados["value"][0]["cotacaoCompra"];
 
         // Quanto $$ você tem ?
         $real = $_REQUEST["din"] ?? 0;
@@ -21,16 +27,12 @@
         $dólar = $real / $cotação;
 
         // Mostrar o resultado
-        // echo "Seus R\$" . number_format($real, 2, ",", ".") . " equivalem a US\$" . number_format($dólar, 2, ",", ".");
-
-        //formatação de moedas com internacionalização!
-        //Biblioteca intl (Internallization PHP)
-
+        
         $padrão = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
 
         echo "<p>Seus " . numfmt_format_currency($padrão, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrão, $dólar, "USD") . "</strong></p>";
 ?>
- <button onclick="javascript:history.go(-1)"> Voltar</button>
+    <button onclick="javascript:history.go(-1)"> Voltar</button>
     </main>   
 </body>
 </html>
